d24d8e2e9b34e68de5d986a9a05bd1a99f49d50c
Movitz Sunar <movitz.sunar@gmail.com>
2023-08-07 01:19:16 +0200

    about + learn

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,5 +1,41 @@
 <template>
-    <div>
-        about
+    <div class="container">
+        <div>
+            <h2 class="text-primary d-flex justify-content-center">SSM Projektledning</h2>
+
+            <div class="row justify-content-center">
+                <div class="col-3 bg-dark rounded p-3 m-2" v-for="x in [0, 0, 0, 0, 0, 0]">
+                    <div class="d-flex justify-content-center flex-column">
+
+                        <img class="rounded"
+                            src="https://upload.wikimedia.org/wikipedia/commons/3/34/Terry_A._Davis_2017.jpg" alt="">
+                        <h5 class="pt-3">Terry Davis</h5>
+                        <i>Eventkoordinator</i>
+                        <nuxt-link to="/challenges/terry">Se Terry's utmaningar</nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row pt-3">
+            <div class="col d-flex flex-row justify-content-center">
+                <img src="~/assets/kodsport.png" alt="Kodsport Logga">
+            </div>
+            <div class="col">
+                <h1 class="text-primary">Föreningen</h1>
+                <p>
+                    <a href="https://www.kodsport.se/">Kodsport</a> är en ideell förening som arbetar för att lyfta Sverige
+                    som
+                    IT-nation. Föreningen arrangerar tävlingar och träningsläger i datorrelaterade sporter, så som
+                    tävlingsprogrammering och Capture The Flag (datorsäkerhet).
+                </p>
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+img {
+    height: 10vw;
+}
+</style>
--- frontend/pages/learn.vue
+++ frontend/pages/learn.vue
@@ -1,5 +1,54 @@
 <template>
-    <div>
-        learn
+    <div class="container">
+        <div>
+            <h1 class="text-primary">Lär dig CTF</h1>
+            <p>Nedan följer några bra resurser för att lära sig om CTF och IT-säkerhetstävlingar.</p>
+            <h3 class="text-primary">Säkerhets-SM föreläsningar</h3>
+            <p>Här är några av Säkerhets-SM:s egna föreläsningar.</p>
+            <ul>
+                <li><a href="https://www.youtube.com/watch?v=jYCN6QyZeeQ">Intro till assembly</a></li>
+                <li><a href="https://www.youtube.com/watch?v=ymlYSeoF6j4">Intro till reverse engineering</a></li>
+                <li><a href="https://www.youtube.com/watch?v=IFjty3suKXg">Intro till pwn</a></li>
+            </ul>
+            <h3 class="text-primary">Youtube</h3>
+            <ul>
+                <li>
+                    <a href="https://www.youtube.com/channel/UClcE-kVhqyiHCcjYwcpfj9w">LiveOverflow</a>: En väldigt
+                    pedagogisk kanal med många bra videos. Nedan är två videos från kanalen.
+                    <ul>
+
+                        <li><a href="https://www.youtube.com/watch?v=2TofunAI6fU">The Secret step-by-step Guide to learn
+                                Hacking</a></li>
+                        <li><a href="https://www.youtube.com/watch?v=8ev9ZX9J45A">What is CTF? An introduction to security
+                                Capture The Flag competitions</a></li><br>
+                    </ul>
+                </li>
+                <li><a href="https://www.youtube.com/user/GynvaelEN">Gynvael</a>: Långa, lite mer avancerade videos, men
+                    väldigt lärorika.</li>
+                <li><a href="https://www.youtube.com/user/ZetaTwo">ZetaTwo</a>: Calle Svensson har många års erfarenhet
+                    av CTF-tävlingar. <a
+                        href="https://www.youtube.com/playlist?list=PLzzz0pYwY0M0u5daM96-QvHagA5v7FhLP">Här</a> går
+                    Calle igenom alla problem från tidigare SSM-tävlingar.</li>
+                <li><a href="https://www.youtube.com/user/RootOfTheNull">John Hammond</a></li>
+            </ul>
+            <h3 class="text-primary">CTF och Wargames</h3>
+            <p>Wargames är som en CTF, fast den är uppe permanent. PicoCTF är en CTF som körs under en period och
+                efteråt så lämnar organisatörerna tävlingen uppe så att folk kan träna. OverTheWire är en wargames-sida.
+            </p>
+            <ul>
+                <li>
+                    <nuxt-link to="/challenges">Säkerhets-SM</nuxt-link>
+                </li>
+                <li><a href="https://picoctf.com/">PicoCTF</a></li>
+                <li><a href="http://overthewire.org/">OverTheWire</a></li>
+            </ul>
+            <h3 class="text-primary">Länksamlingar och anteckningar</h3>
+            <ul>
+                <li><a href="https://gist.github.com/ZetaTwo/40976c9ed8b9abb81e44c872b3a68551">ZetaTwo - tools.md</a>
+                </li>
+                <li><a href="https://pequalsnp-team.github.io/cheatsheet/steganography-101">Steganography 101</a></li>
+                <li><a href="https://github.com/InfoSecIITR/reading-material">InfoSecIITR - Reading material</a></li>
+            </ul>
+        </div>
     </div>
 </template>
