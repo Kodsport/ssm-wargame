4699c7bd963938a9d0ca52d30c962dc2746362a7
mattiasgrenfeldt <9297883+mattiasgrenfeldt@users.noreply.github.com>
2023-11-03 23:36:17 +0100

    /challenges is mobile friendly

--- frontend/pages/challenges.vue
+++ frontend/pages/challenges.vue
@@ -1,59 +1,54 @@
 <template>
-    <div class="container-fluid">
-        <h1 class="text-primary">Utmaningar</h1>
-        <p>
-            Här kan du öva på utmaningar från tidigare års upplagor av Säkerhets-SM.
-        </p>
+    <h1 class="text-primary">Utmaningar</h1>
+    <p>
+        Här kan du öva på utmaningar från tidigare års upplagor av Säkerhets-SM.
+    </p>
 
-        <div class="row pt-4">
-
-            <div class="col-3">
-                <h3>Filtrera</h3>
-                <div>
-                    <div class="form-group">
-                        <label>Kategori</label>
-                        <select class="form-control" v-model="challFilter.categoryFilter">
-                            <option value="">alla</option>
-                            <option class="text-lowercase" v-for="catx in categories" :value="catx">{{ catx }}
-                            </option>
-                        </select>
-                    </div>
-                    <div v-if="challs.events.length" class="form-group pt-3">
-                        <label>Tävling</label>
-
-                        <div class="form-check" v-for="event in challs.events">
-                            <input type="checkbox" class="form-check-input" v-model="challFilter.eventFilter[event.id]">
-                            <label class="form-check-label"
-                                @click="challFilter.eventFilter[event.id] = !challFilter.eventFilter[event.id]">{{
-                                    event.name }}</label>
-                        </div>
-                    </div>
-                </div>
+    <div class="d-flex flex-column flex-lg-row pt-4">
+        <div class="ssm-filter pe-lg-3">
+            <h3>Filtrera</h3>
+            <div class="form-group">
+                <label>Kategori</label>
+                <select class="form-control pointer" v-model="challFilter.categoryFilter">
+                    <option value="">alla</option>
+                    <option class="text-lowercase" v-for="catx in categories" :value="catx">
+                        {{ catx }}
+                    </option>
+                </select>
             </div>
+            <div v-if="challs.events.length" class="form-group pt-3">
+                <label>Tävling</label>
 
-            <div class="col">
-                <div class="row" v-for="category in categories">
-                    <div
-                        v-if="(challFilter.categoryFilter == '' || category == challFilter.categoryFilter) && challenges.filter(c => c.category == category).length">
-
-                        <div class="text-primary border-primary border-bottom border-3 pb-2 me-3">
-                            <h4 class="text-lowercase">{{ category }}</h4>
-                        </div>
-
-                        <div class="row row-cols-3 row-cols-xl-5 g-4 mt-0 mb-2">
-                            <div class="col" v-for="chall in challenges.filter(c => c.category == category)">
-                                <ChallengePreview @click="nav(chall.slug)" :chall="chall" />
-                            </div>
-                        </div>
-                    </div>
+                <div class="form-check" v-for="event in challs.events">
+                    <input type="checkbox" class="form-check-input pointer" v-model="challFilter.eventFilter[event.id]">
+                    <label
+                        class="form-check-label pointer"
+                        @click="challFilter.eventFilter[event.id] = !challFilter.eventFilter[event.id]"
+                    >
+                        {{ event.name }}
+                    </label>
                 </div>
             </div>
         </div>
-        <NuxtPage />
+
+        <div class="flex-grow-1 d-flex flex-column">
+            <template v-for="category in categories">
+                <div v-if="(challFilter.categoryFilter == '' || category == challFilter.categoryFilter) && challenges.filter(c => c.category == category).length">
+                    <div class="text-primary border-primary border-bottom border-3 pt-2 pb-2">
+                        <h4 class="text-lowercase">{{ category }}</h4>
+                    </div>
+
+                    <div class="ssm-grid pt-3 pb-2">
+                        <div v-for="chall in challenges.filter(c => c.category == category)">
+                            <ChallengePreview class="pointer" @click="nav(chall.slug)" :chall="chall" />
+                        </div>
+                    </div>
+                </div>
+            </template>
+        </div>
     </div>
+    <NuxtPage />
 </template>
-
-
 
 <script setup lang="ts">
 import { useChallengeStore } from '../store/challenges'
@@ -101,6 +96,22 @@ const challenges = computed(() => {
 function nav(slug: string) {
     router.push(`/challenges/${slug}`)
 }
-
 </script>
 
+<style scoped>
+    .pointer {
+        cursor: pointer;
+    }
+    .ssm-grid {
+        display: grid;
+        /* Magic auto-sizing grid: https://css-tricks.com/auto-sizing-columns-css-grid-auto-fill-vs-auto-fit/ */
+        grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+        gap: 1rem;
+    }
+    /* Bootstrap large */
+    @media (min-width: 992px) {
+        .ssm-filter {
+            width: 21rem;
+        }
+    }
+</style>
