66ae8aabd7e698284bfa0e3c7b4cbf4eecb90b24
Movitz Sunar <movitz.sunar@gmail.com>
2023-12-30 05:36:49 +0100

    nofollow service link

--- frontend/components/MonthlyChallenge.vue
+++ frontend/components/MonthlyChallenge.vue
@@ -14,7 +14,7 @@
 
         <div class="d-flex pb-2" v-for="service in props.chall.services">
             <span class="material-symbols-outlined pe-2">router</span>
-            <a v-if="service.hyperlink" :href="service.user_display" target=”_blank”>{{ service.user_display }}</a>
+            <a v-if="service.hyperlink" :href="service.user_display" target="_blank">{{ service.user_display }}</a>
             <span v-else>{{ service.user_display }}</span>
         </div>
 
