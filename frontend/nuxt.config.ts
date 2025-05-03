5f14e17a05f4b462e7ce540d19d505016d4457f8
Liam Thorell <liamthorell18@gmail.com>
2025-05-03 18:26:52 +0200

    Formatting and defineModel fix in nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,44 +3,52 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   pages: true,
   modules: [
-    '@pinia/nuxt',
+    "@pinia/nuxt",
     // 'nuxt-security' // TODO fix later
   ],
-  css: [
-    '~/assets/scss/main.scss'
-  ],
+  css: ["~/assets/scss/main.scss"],
   app: {
     head: {
       link: [
         {
-          rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        }
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
+        },
       ],
-      title: 'Säkerhets-SM',
+      title: "Säkerhets-SM",
       meta: [
         {
-          name: 'description', content: 'Säkerhets-SM är en tävling inom cybersäkerhet för grundskolan och gymnasiet'
-        }
+          name: "description",
+          content:
+            "Säkerhets-SM är en tävling inom cybersäkerhet för grundskolan och gymnasiet",
+        },
       ],
       htmlAttrs: {
-        lang: 'se'
+        lang: "se",
       },
       script: [
         {
           // <script defer src="https://analytics.sakerhetssm.se/script.js" data-website-id="ec04f82f-8090-4b5a-b404-5a07099e1d78"></script>
           defer: true,
-          src: 'https://analytics.sakerhetssm.se/script.js',
-          'data-website-id': 'ec04f82f-8090-4b5a-b404-5a07099e1d78'
-        }
-      ]
-    }
+          src: "https://analytics.sakerhetssm.se/script.js",
+          "data-website-id": "ec04f82f-8090-4b5a-b404-5a07099e1d78",
+        },
+      ],
+    },
   },
   runtimeConfig: {
     public: {
-      apiBase: 'set via env'
-    }
+      apiBase: "set via env",
+    },
   },
   routeRules: {
-    '/admin/**': { ssr: false },
-  }
-})
+    "/admin/**": { ssr: false },
+  },
+  vite: {
+    vue: {
+      script: {
+        defineModel: true,
+      },
+    },
+  },
+});
