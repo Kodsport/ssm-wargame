fb5570a6a7875493cedf65560d918db0cd4d5564
alanoo <alsalihi.allan@gmail.com>
2025-07-07 15:31:55 +0200

    oops

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000",
+      apiBase: "set via env",
     },
   },
   routeRules: {
