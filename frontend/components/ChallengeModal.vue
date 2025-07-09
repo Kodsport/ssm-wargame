af526a989d5118dde1181bd61520e6947698c121
alanoo <alsalihi.allan@gmail.com>
2025-07-09 14:54:41 +0200

    revert colors & redesign

--- frontend/components/ChallengeModal.vue
+++ frontend/components/ChallengeModal.vue
@@ -7,11 +7,8 @@
                     <h2 class="modal-title text-primary fw-bold">{{ props.chall.title }}</h2>
                 </div>
                 <div class="d-flex justify-content-center">
-                    <div class="badge me-1" :class="getCategoryClass()">{{ props.chall.category }}</div>
-                    <span v-if="event" class="badge bg-primary me-1">{{ (event as any).name }}</span>
-<!--                     <span class="difficulty-badge me-1" :class="getDifficultyClass()">
-                        {{ getDifficulty() }}
-                    </span> -->
+                    <div class="badge bg-primary me-1">{{ props.chall.category }}</div>
+                    <span v-if="event" class="badge bg-primary">{{ event.name }}</span>
                 </div>
 
                 <h5>
@@ -99,53 +96,12 @@ const props = defineProps(['chall'])
 const emit = defineEmits(['back'])
 
 const modal = ref(null)
-const event = computed(() => store.events.find((e: any) => e.id == props.chall.ctf_event_id))
+const event = computed(() => store.events.find(e => e.id == chall.ctf_event_id))
 
-const chall = computed(() => store.getBySlug(route.params.slug as string))
+const chall = computed(() => store.getBySlug(route.params.slug))
 
 const flag = ref("")
 const warn = ref(false)
-
-// fixar voting senare, detta kan dock användas ifall man vill.
-/* const getDifficulty = () => {
-    const score = props.chall.score || props.chall.static_score || 0
-    if (score <= 150) return 'enkel'
-    if (score <= 300) return 'medel'
-    return 'svår'
-}
-
-const getDifficultyClass = () => {
-    const difficulty = getDifficulty()
-    return {
-        'difficulty-easy': difficulty === 'enkel',
-        'difficulty-medium': difficulty === 'medel',
-        'difficulty-hard': difficulty === 'svår',
-    }
-} */
-
-const getCategoryClass = () => {
-    const category = props.chall.category?.toLowerCase()
-    switch (category) {
-        case 'pwn':
-            return 'category-pwn'
-        case 'crypto':
-            return 'category-crypto'
-        case 'web':
-            return 'category-web'
-        case 'forensics':
-            return 'category-forensics'
-        case 'misc':
-            return 'category-misc'
-        case 'osint':
-            return 'category-osint'
-        case 'rev':
-        case 'reverse':
-        case 'reversing':
-            return 'category-rev'
-        default:
-            return 'bg-secondary'
-    }
-}
 
 async function submitFlag() {
     try {
@@ -182,64 +138,6 @@ function goBack(event) {
 </script>
 
 <style scoped>
-.difficulty-badge {
-    padding: 0.15rem 0.35rem;
-    border-radius: 0.2rem;
-    font-size: 0.65rem;
-    font-weight: 600;
-    border: 1px solid #00000077 !important;
-}
-
-.difficulty-easy {
-    background-color: #1b9a41;
-    color: #ffffff;
-}
-
-.difficulty-medium {
-    background-color: #ddb531;
-    color: #ffffff;
-}
-
-.difficulty-hard {
-    background-color: #8e3039;
-    color: #ffffff;
-}
-
-.category-pwn {
-    background-color: #b01d2b;
-    color: white;
-}
-
-.category-crypto {
-    background-color: #ffbf00;
-    color: #ffffff;
-}
-
-.category-web {
-    background-color: #2281a7;
-    color: #ffffff;
-}
-
-.category-forensics {
-    background-color: #144690;
-    color: white;
-}
-
-.category-misc {
-    background-color: #6f42c1;
-    color: white;
-}
-
-.category-osint {
-    background-color: #6c757d;
-    color: white;
-}
-
-.category-rev {
-    background-color: #b47523;
-    color: white;
-}
-
 .author:after {
     content: ",";
     padding-right: 0.5em;
