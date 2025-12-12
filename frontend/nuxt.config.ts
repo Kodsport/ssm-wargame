9b576057a2af7ef6f6dd0431918ece0de8d6d058
alanoo <alsalihi.allan@gmail.com>
2025-12-12 20:47:59 +0100

    fix till analytics script.js

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -30,14 +30,7 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "se",
       },
-      script: [
-        {
-          // <script defer src="https://analytics.sakerhetssm.se/script.js" data-website-id="ec04f82f-8090-4b5a-b404-5a07099e1d78"></script>
-          defer: true,
-          src: "https://analytics.sakerhetssm.se/script.js",
-          "data-website-id": "ec04f82f-8090-4b5a-b404-5a07099e1d78",
-        },
-      ],
+      script: [],
     },
   },
   runtimeConfig: {
--- frontend/plugins/bootstrap.client.ts
+++ frontend/plugins/bootstrap.client.ts
@@ -2,4 +2,9 @@ import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
 
 export default defineNuxtPlugin(nuxtApp => {
     nuxtApp.provide('bootstrap', bootstrap)
+    nuxtApp.hooks.hook('app:mounted', () => {
+        const s = Object.assign(document.createElement('script'), {async: true, src: 'https://analytics.sakerhetssm.se/script.js'});
+        s.dataset.websiteId = 'ec04f82f-8090-4b5a-b404-5a07099e1d78';
+        document.body.append(s);
+    });
 })
